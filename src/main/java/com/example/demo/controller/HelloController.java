b3e8cec7d6ea58ac590443fbed4e94239e7ce658
Panachai <panachai.ny@gmail.com>
2020-01-21 00:12:38 +0700

    Update HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,10 @@
 package com.example.demo.controller;
 
+import java.io.File;
+
+import com.example.demo.model.Car;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +14,14 @@ public class HelloController {
 	@RequestMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
+	}
+
+	@RequestMapping("/test")
+	public String test() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Car car = new Car("yellow", "renault");
+		objectMapper.writeValue(new File("target/car.json"), car);
+		return "";
 	}
 
 }
